d56e6bf7513dc85efed355fc930353cb353e9a28
Eric Chang <89112536+Eric504060304@users.noreply.github.com>
2021-10-02 17:54:41 +0800

    檔案上傳

---
+++ Assets/C#/Playermovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rig;
+    public AudioSource aud;
+    public Animator ani;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
