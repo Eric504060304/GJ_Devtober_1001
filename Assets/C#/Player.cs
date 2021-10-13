c10465b1c1e3b78b04aef523e3aa235f5a26d2d3
Eric Chang <89112536+Eric504060304@users.noreply.github.com>
2021-10-13 20:00:13 +0800

    20211013進度: 場景物件設置、人物角色設計改動、人物操作

--- Assets/C#/Player.cs
+++ Assets/C#/Player.cs
@@ -4,59 +4,27 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5f;
 
+    public Rigidbody2D rig;
+    public Camera cam;
 
-
-    public Rigidbody2D rb;
-    private AudioSource aud;
-    private Animator ani;
-
-
-
-    
-    //private BoxCollider2D boxCollider;
-    public float moveSpeed = 5f;
     Vector2 movement;
+    Vector2 mousePos;
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rig.MovePosition(rig.position + movement * speed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rig.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 
-        #region 移動的另外一種寫法
-        /* 移動寫法
-        //Reset MoveDelta
-        //moveDelta = new Vector3(x, y, 0);
-        //Swap sprite direction, wether you're going right or left
-        if (moveDelta.x > 0)
-            transform.localScale = Vector3.one;
-        else if (moveDelta.x < 0)
-            transform.localScale = new Vector3(-1, 1, 1);
-        //Make sure we can move in this direction, by castig a box tthere first, if the box returns null, we're free to move 
-        hit = Physics2D.BoxCast
-            (transform.position, boxCollider.size, 0,
-            new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime),
-            LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            //Make this thing move!
-            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
-        }
-
-        hit = Physics2D.BoxCast
-            (transform.position, boxCollider.size, 0,
-            new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime),
-            LayerMask.GetMask("Actor", "Blocking"));
-        if (hit.collider == null)
-        {
-            //Make this thing move!
-            transform.Translate(moveDelta.x, 0 * Time.deltaTime, 0, 0);
-        }
-        */
-        #endregion
-
+        rig.rotation = angle;
     }
 }
---
+++ Assets/C#/Shooting.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject BulletPrefab;
+
+    public float BulletForce = 20f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject Bullect = Instantiate(BulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D rig =  Bullect.GetComponent<Rigidbody2D>();
+        rig.AddForce(firePoint.up * BulletForce, ForceMode2D.Impulse);
+    }
+}
