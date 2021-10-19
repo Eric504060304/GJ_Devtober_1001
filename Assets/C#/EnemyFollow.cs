8581f43563ea5020af5e702c7389d38e835d55d9
Eric Chang <89112536+Eric504060304@users.noreply.github.com>
2021-10-19 20:59:50 +0800

    完成被怪物追殺到牆角的設定

--- Assets/C#/EnemyFollow.cs
+++ Assets/C#/EnemyFollow.cs
@@ -16,9 +16,25 @@ public class EnemyFollow : MonoBehaviour
 
     void Update()
     {
-        if (Vector2.Distance(transform.position, playerPos.position) > 0.3f)
+        if (Vector3.Distance(transform.position, playerPos.position) > 0.3f)
         {
-            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+            MoveTowardsPlayer();
+            RotateTowardsPlayer();
         }
+    }
+
+    private void MoveTowardsPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+    }
+
+    private void RotateTowardsPlayer()
+    {
+        var offset = 90f;
+        Vector2 direction = playerPos.position - transform.position;
+        direction.Normalize();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(Vector3.back * (angle + offset));
+
     }
 }
